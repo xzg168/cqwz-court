dffe5f03a659681640ef1940f5568546b3dc52b1
xzg168 <741491161@qq.com>
2021-02-05 11:39:32 +0800

    修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -69,28 +69,26 @@ export default {
       this.form.validateFields((err, values) => {
         if (!err) {
           console.log("Received values of form: ", values);
-          this.$store.dispatch("setUserInfo", {
-            account: "admin",
-            create_time: "2021-02-02 14:47:35",
-            id: 1,
-            is_flag: 1,
-            is_type: 1,
-            mailbox: "123@qq.com",
-            name: "管理员",
-            password: "123456"
-          });
+          // this.$store.dispatch("setUserInfo", {
+          //   account: "admin",
+          //   create_time: "2021-02-02 14:47:35",
+          //   id: 1,
+          //   is_flag: 1,
+          //   is_type: 1,
+          //   mailbox: "123@qq.com",
+          //   name: "管理员",
+          //   password: "123456"
+          // });
           this.$http.post("/admin/login", values).then(res => {
             if (res.code === 200) {
               this.$store.dispatch("setUserInfo", res.data);
               setToken(res.data.id);
-              window.sessionStorage.setItem("token", "123");
+              window.sessionStorage.setItem("token", res.data.id);
               this.$router.push("/");
-              setToken("123");
             } else {
               // this.$message.erro(res.message);
             }
           });
-
         }
       });
     }
