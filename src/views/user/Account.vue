651d452f4b16a68623f1dbe8c8a231c1684f7316
xzg168 <741491161@qq.com>
2021-02-05 11:09:58 +0800

    增加

--- src/views/user/Account.vue
+++ src/views/user/Account.vue
@@ -241,7 +241,9 @@ export default {
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
+  created() {
+    this.getData();
+  },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {},
   beforeCreate() {}, //生命周期 - 创建之前
